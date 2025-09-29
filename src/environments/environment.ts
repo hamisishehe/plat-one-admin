3bbd8980f35f24fb0fb58edba22de0c4a3dcc4cf
hamisishehe <hamisishehe@gmail.com>
2025-09-29 08:16:34 +0300

    v22

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://plat-one-back-production.up.railway.app/api',
+  // baseUrl: 'https://plat-one-back-production.up.railway.app/api',
   // baseUrl: 'http://localhost:8081/api',
 };
